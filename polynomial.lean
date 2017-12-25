3ab5c70cf14948764cbdf7251a8ca4d57abb7fba
Siva Somayyajula <sks266@cornell.edu>
2017-12-25 15:58:35 -0500

    better polynomials

--- polynomial.lean
+++ polynomial.lean
@@ -1,22 +1,5 @@
 import .seq
 
-inductive is_polynomial {R} [ring R] : set (R → R)
-| intro {n} (a : seq R n) : is_polynomial (λ x, seq.sum (λ i, a i * x /- ^ i -/))
-
-def P (R) [ring R] := subtype (@is_polynomial R _)
-
-def zero {R} [ring R] : P(R) :=
-⟨λ _, 0, begin
-  let a : seq R 1 := seq.const 0,
-  let h : ∀ x, seq.sum (λ i, a i * x /- ^ i -/) = 0 :=
-    by {intro x, show 0 * x /- ^ i -/ + 0 = 0, simp},
-  rw ←funext h,
-  apply is_polynomial.intro
-end⟩
-
-instance {R} [ring R] {n} : has_coe (seq R n) (P R) :=
-⟨λ a, ⟨λ x, seq.sum (λ i, a i * x /- ^ i -/), is_polynomial.intro a⟩⟩
-
 inductive with_bottom (α)
 | bot : with_bottom
 | intro : α → with_bottom
@@ -33,9 +16,16 @@ instance {α} [has_le α] : has_le (with_bottom α) :=
 def fin.max {n} : fin (n + 1) :=
 ⟨n, nat.le_refl _⟩
 
-inductive has_degree {R} [ring R] : P(R) → with_bottom ℕ → Prop
-| intro_zero                    : has_degree zero -∞
-| intro {n} (a : seq R (n + 1)) : a fin.max ≠ 0 → has_degree a n
+-- A function R → R is a polynomial of degree...
+inductive is_polynomial_of_degree {R} [ring R] : (R → R) → with_bottom ℕ → Prop
+| intro_zero :
+    is_polynomial_of_degree (λ _, 0) -∞
+| intro_sum {n} (a : seq R (n + 1)) :
+    a fin.max ≠ 0 → is_polynomial_of_degree (λ x, seq.sum (λ i, a i * x /- ^ i -/)) n
 
-def P_deg_le (n : with_bottom ℕ) (R) [r : ring R] :=
-@subtype (P(R)) (λ p, ∃ m ≤ n, has_degree p m)
+def P (R) [ring R] : set (R → R) :=
+λ f, ∃ n, is_polynomial_of_degree f n
+
+def P_deg_le (n) (R) [ring R] : set (R → R) :=
+λ f, ∃ m ≤ n, is_polynomial_of_degree f m
+
